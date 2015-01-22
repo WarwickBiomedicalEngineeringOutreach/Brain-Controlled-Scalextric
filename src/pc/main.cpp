4c43c3464a1d7443ad6b73d2c315cc1e642c1295
Frantisek Foston <frankiefoston@gmail.com>
2015-01-22 17:00:25 +0000

    added code for second controller

--- src/pc/main.cpp
+++ src/pc/main.cpp
@@ -10,9 +10,9 @@ const uint8_t maxSpeed= 70;
 int main(int argc, char *argv[]) {
     QCoreApplication app(argc, argv);
 
-    if (app.arguments().size() != 3) {
+    if (app.arguments().size() != 4) {
         qDebug() << "Invalid Arguments provided";
-        qDebug() << "Correct arguement syntax is [MindWaveSerialPort] [ArduinoSerialPort]";
+        qDebug() << "Correct arguement syntax is [MindWaveSerialPort1] [MindWaveSerialPort2] [ArduinoSerialPort]";
         return 1;
     }
 
@@ -21,8 +21,13 @@ int main(int argc, char *argv[]) {
         return 2;  // failed to open Serial Port with MindWave controller
     }
 
+    MindWaveController controller2;
+    if (controller2.initController(app.arguments().at(2))) {
+        return 2;  // failed to open Serial Port with MindWave controller
+    }
+
     ArduinoInterface arduino;
-    if (arduino.init(app.arguments().at(2))) {
+    if (arduino.init(app.arguments().at(3))) {
         return 3; // failed to connect to the arduino's serial port
     }
 
@@ -45,28 +50,22 @@ int main(int argc, char *argv[]) {
         arduino.write(outputdata);
     });
 
-    QTimer *controller2 = new QTimer();
-    uint8_t controller2Value = 50;
-    controller2->setInterval(1000);
-
-    arduino.connect(controller2, &QTimer::timeout, [&](){
+    arduino.connect(&controller2, &MindWaveController::attentionDataChanged,
+                       [&](uint16_t data){
         QByteArray outputdata;
         outputdata.append(0x20);
 
-        if (controller2Value > maxSpeed) {
-            controller2Value = 50;
+        if (data > maxSpeed) {
+            outputdata.append(maxSpeed);
+        } else {
+            outputdata.append(data&0xFF);
         }
-        outputdata.append(controller2Value);
-
-        controller2Value++;
 
         qDebug() << "Player Code: " << static_cast<uint8_t>(outputdata.at(0))
                  << "Player Level: "<< static_cast<uint8_t>(outputdata.at(1));
 
         arduino.write(outputdata);
     });
-
-    controller2->start();
 
     return app.exec(); // start event loop
 }
